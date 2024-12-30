41205b0b35faf1475186a1968c8d97cf93dc5861
Dhruvin Mehta <dhruvin@browserstack.com>
2024-12-31 00:49:28 +0530

    Update bstack_test_add_to_cart.js

--- tests/bstack_test_add_to_cart.js
+++ tests/bstack_test_add_to_cart.js
@@ -14,5 +14,5 @@ test('BStackDemo test add to cart', async ({ page }) => {
   const productInCart = await page.textContent('#__next > div > div > div.float-cart.float-cart--open > div.float-cart__content > div.float-cart__shelf-container > div > div.shelf-item__details > p.title');
   
   // check if product in cart is same as one added
-  expect(productInCart).toEqual(productToAdd);
+  expect(productInCart).toEqual("Failing condition");
 });
