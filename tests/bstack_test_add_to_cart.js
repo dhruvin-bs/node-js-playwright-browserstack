0e94149ed61c4051f0501081ce7a616e4ea7cae3
Dhruvin Mehta <dhruvin@browserstack.com>
2024-12-31 11:19:39 +0530

    Update bstack_test_add_to_cart.js

--- tests/bstack_test_add_to_cart.js
+++ tests/bstack_test_add_to_cart.js
@@ -1,6 +1,6 @@
 const { expect, test } = require('@playwright/test');
 
-test('BStackDemo test add to cart', async ({ page }) => {
+test('BStackDemo test add to cart', { tag: ['@app', '@healthcheck', '@performance', '@visual'] }, async ({ page }) => {
   // visit the site
   await page.goto('https://bstackdemo.com/');
 
